5f7f5d30ce12593a2bc7aa1a583209830f408bf3
Bill McCutchan <bmccutchanjr@gmail.com>
2018-10-26 20:29:43 -0400

    finished style and cleaned up some code

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,22 +1,7 @@
 var ting = new Audio("ting.mp3");
 
-// function getQuestion()
-// {   // Returns an array of .question-div with attribute unanswered=true
-// 
-//     return questions = $(".question-div[unanswered='true'");
-// }
-
 $(document).ready (function ()
-{   
-
-// var questions = $(".question-div[unanswered='true'");
-// console.log("ready");
-// console.log("questions: ", questions);
-// console.log("which: ", $(questions[0]).attr("which"));
-//     var questions = getQuestions ();
-
-// console.log ("survey.html");
-    // The page has just loaded -- the user has not had an opportunity to interact with it yet.  Make
+{   // The page has just loaded -- the user has not had an opportunity to interact with it yet.  Make
     // the first survey question visible...
     $(".question-div[which=0]").css("display", "block");
 
@@ -26,41 +11,10 @@ $(document).ready (function ()
 
         event.preventDefault ();
 
-// console.log("change");
-// console.log("event: ", event);
-// console.log("target: ", event.currentTarget);
-// console.log("which one: ", $(event.currentTarget).attr("select"));
         var valid = true;
         var question = $(event.currentTarget);
         var which = question.attr("which");
-// console.log("which: ", which);
 
-//         var option = parseInt($(event + " option:selected").val());
-//         var option = parseInt($(".chosen-select[which=" + which + "] option:selected").val());
-// console.log("option: ", option);
-//         if (isNaN(option)) valid = false;
-    
-//         if ((option < 1) || (option > 5))
-//         {   // The survey responses are predefined in a <select>.  Possible <options> are a null
-//             // string or values "1", "2", "3", "4" or "5".  This should never happen, but should it
-//             // happen it needs to be handled.
-        
-//             valid = false;
-//         }
-
-//         if (!valid)
-//         {   $("question-text[which=" + which + "]").css ("color", "red");
-//         }
-//         else
-//         {   // The response was valid.  Hide this question and display the next.
-
-//             $(".question-div[which=" + which + "]").css("display", "none");
-            
-//             if (which < 9)
-//             {   which++;
-//                 $(".question-div[which=" + which + "]").css("display", "block");
-//             }
-//         }
         // The survey responses are predefined in a <select> and this function is called by the on change
         // event listener.  If the event fired, the associated <select> was changed and the value should
         // always be valid.  No need to validate the response here, just display the next question.
@@ -92,7 +46,6 @@ $(document).ready (function ()
     $(".submit-button").click(function (event)
     {   // event listener for sunmit button
 
-// console.log("on click")
         event.preventDefault();
 
         // First, set the color of all the labels and questions to black.  It's possible this is not the
@@ -123,12 +76,10 @@ $(document).ready (function ()
             // have been answered.
 
             var option = $(".chosen-select[which=" + i + "] option:selected").val();
-// console.log ($(".chosen-select[value=" + i + "]"));
-// console.log ($(".chosen-select[value=" + i + "] option:selected"));
-// console.log ("option: ", option);
+
             if (!option)
             {   valid = false;
-// console.log ("question #", i, " not answered");
+
                 $(".question-text[which=" + i + "]").css ("color", "red");
             }
             else
@@ -143,12 +94,8 @@ $(document).ready (function ()
                 "scores":   scores
             }
 
-console.log ("data: ", data);
-
             $.post ("http://localhost:4000/api/friends/", data, function (response, status)
-            {
-console.log ("posted and responded");            
-
+            {   // format the data received from the server and display on the screen
                 var rDiv = $(".response-data")
                 rDiv.empty();
 
@@ -161,16 +108,9 @@ console.log ("posted and responded");
                 .css ("hieght", 300)
                 .css ("width", 200);
 
-                var close = $("<button>");
-                close
-                .addClass("close")
-                .attr ("value", "close")
-                .text ("CLOSE");
-
                 rDiv
                 .append (title)
-                .append (image)
-                .append (close);
+                .append (image);
 
                 $(".response").css("display", "flex");
             })
@@ -178,10 +118,8 @@ console.log ("posted and responded");
         }
     });
 
-    $(".response-data").on ("click", ".close", function(event)
+    $(".close-button").click(function(event)
     {   // event handler for "close" button in response <div>
-
-//         $(".response").hide();
 
         $(".response").css("display", "none");
     })
